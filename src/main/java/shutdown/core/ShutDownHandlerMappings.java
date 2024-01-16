b812432ade43cc054398ade788acd5a8b13de7ac
ecsimsw <ecsimsw@gmail.com>
2024-01-16 09:10:17 +0900

    javax -> jakarta

--- src/main/java/shutdown/core/ShutDownHandlerMappings.java
+++ src/main/java/shutdown/core/ShutDownHandlerMappings.java
@@ -1,6 +1,7 @@
 package shutdown.core;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.*;
 
 public class ShutDownHandlerMappings {
