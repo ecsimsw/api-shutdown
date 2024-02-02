1cfa57ae553bae6eabec84e8d7e437267a59f8d6
ecsimsw <ecsimsw@gmail.com>
2024-02-02 20:16:15 +0900

    test bean factory list

--- src/main/java/shutdown/core/ShutDownFilterRegister.java
+++ src/main/java/shutdown/core/ShutDownFilterRegister.java
@@ -1,6 +1,8 @@
 package shutdown.core;
 
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -10,17 +12,24 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class ShutDownFilterRegister implements BeanFactoryPostProcessor, EnvironmentAware {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShutDownFilterRegister.class);
+
     private Environment environment;
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         var globalConfig = getGlobalConfiguration(beanFactory);
+
+        Arrays.stream(beanFactory.getBeanDefinitionNames())
+            .forEach(it -> LOGGER.info(it));
+
         for (var controller : shutDownControllerTypes(beanFactory)) {
             var annotated = ShutDownAnnotated.of(controller);
             if(isShutDownCondition(annotated, beanFactory)) {
